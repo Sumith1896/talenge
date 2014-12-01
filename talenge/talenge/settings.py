ff9aca7fa4119e62e22a3c112c25d39bcdf9995f
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2014-12-01 15:13:38 +0530

    Added DB creds, template/static

--- talenge/talenge/settings.py
+++ talenge/talenge/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__)+'/../')
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,11 +57,24 @@ WSGI_APPLICATION = 'talenge.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', 	# Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'talenge',                      				# Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'talenge',
+            'PASSWORD': 'talenge',
+            'HOST': 'localhost',                      				# Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+            'PORT': '',                      						# Set to empty string for default.
+        }
 }
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
